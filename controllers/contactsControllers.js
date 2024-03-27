1046f2a9775d5425844ef71751a651bd0d563a7d
Elizaveta139 <elizavetarevina139@gmail.com>
2024-03-27 13:36:19 +0200

    validate

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -48,10 +48,6 @@ export const deleteContact = async (req, res, next) => {
 
 export const createContact = async (req, res, next) => {
   try {
-    const { error } = createContactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
     const result = await addContact(req.body);
     res.status(201).json(result);
   } catch (error) {
@@ -61,12 +57,8 @@ export const createContact = async (req, res, next) => {
 
 export const updateContact = async (req, res, next) => {
   try {
-    const { error } = updateContactSchema.validate(req.body);
     if (!req.body || Object.keys(req.body).length === 0) {
       throw HttpError(400, "Body must have at least one field");
-    }
-    if (error) {
-      throw HttpError(400, error.message);
     }
 
     const { id } = req.params;
