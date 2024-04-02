b731f4985438e3afbda1a0ec6645d2ff95c1fd11
Elizaveta139 <elizavetarevina139@gmail.com>
2024-04-02 21:41:28 +0300

    hw

--- app.js
+++ app.js
@@ -24,11 +24,14 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
+const { DB_HOST, PORT = 3000 } = process.env;
+
 mongoose
-  .connect(process.env.DB_HOST)
+  .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Database connection successful");
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
   .catch((err) => {
