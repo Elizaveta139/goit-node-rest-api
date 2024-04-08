e1e130c8005a24bc2552a277cf0df430afa0fa0f
Elizaveta139 <elizavetarevina139@gmail.com>
2024-04-08 10:31:38 +0300

    +

--- schemas/contactsSchemas.js
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().min(6).required(),
-});
-
-export const updateContactSchema = Joi.object({});
